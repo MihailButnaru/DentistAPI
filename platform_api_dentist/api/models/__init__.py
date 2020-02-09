d5456c4ebc384f5fc16c4c23c8ca408788671d16
Mihail Butnaru <butnarum@hotmail.com>
2020-02-09 19:46:11 +0000

    models fix

--- platform_api_dentist/api/models/__init__.py
+++ platform_api_dentist/api/models/__init__.py
@@ -2,7 +2,7 @@ from .patient_model import Patient, Address
 from .payment_model import Payment
 from .dentist_model import DentistSpecialist
 from .treatment_models import Treatment
-from .appointment_card import Appointments, AppointmentCard
+from .appointment_model import Appointments, AppointmentCard
 
 __all__ = [
     Patient,
